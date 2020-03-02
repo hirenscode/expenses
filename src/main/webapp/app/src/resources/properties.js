ca717dc7e76aaec06f352bfb39fd620289fa2989
Hiren Parmar <hiren.parmar@workday.com>
2020-03-02 11:47:49 -0800

    Updated pom.xml with npm to use run build, instead of just build, and Endpoint to heroku's domain.

--- src/main/webapp/app/src/resources/properties.js
+++ src/main/webapp/app/src/resources/properties.js
@@ -1,5 +1,11 @@
-export const BACKEND_DOMAIN = "http://localhost:8080";
-export const BACKEND_API_PATH = `${BACKEND_DOMAIN}/api/v1`;
+export const BACKEND_PROTOCOL_HTTPS = "https";
+export const BACKEND_PROTOCOL_HTTP = "http";
+export const BACKEND_DOMAIN_HEROKU = "expenses-live.herokuapp.com";
+export const BACKEND_DOMAIN_LOCAL = "localhost";
+export const BACKEND_PORT_LOCAL = "8080";
+export const BACKEND_API_PATH_LOCAL = `${BACKEND_PROTOCOL_HTTP}://${BACKEND_DOMAIN_LOCAL}:${BACKEND_PORT_LOCAL}/api/v1`;
+export const BACKEND_API_PATH_HEROKU = `${BACKEND_PROTOCOL_HTTPS}://${BACKEND_DOMAIN_HEROKU}/api/v1`;
+export const BACKEND_API_PATH = BACKEND_API_PATH_HEROKU;
 const FAKE_BACKEND = true;
 export let IS_BACKEND_HEALTHY = () => {
     if (FAKE_BACKEND) return false;
