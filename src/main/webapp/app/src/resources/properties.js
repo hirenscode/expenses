34c399a0edcea85d6e3afe8db6555601a3531848
Hiren Parmar <hiren.parmar@workday.com>
2020-03-04 20:15:41 -0800

    trying to make app prod ready.

--- src/main/webapp/app/src/resources/properties.js
+++ src/main/webapp/app/src/resources/properties.js
@@ -1,11 +1,4 @@
-export const BACKEND_PROTOCOL_HTTPS = "https";
-export const BACKEND_PROTOCOL_HTTP = "http";
-export const BACKEND_DOMAIN_HEROKU = "expenses-live.herokuapp.com";
-export const BACKEND_DOMAIN_LOCAL = "localhost";
-export const BACKEND_PORT_LOCAL = "8080";
-export const BACKEND_API_PATH_LOCAL = `${BACKEND_PROTOCOL_HTTP}://${BACKEND_DOMAIN_LOCAL}:${BACKEND_PORT_LOCAL}/api/v1`;
-export const BACKEND_API_PATH_HEROKU = `${BACKEND_PROTOCOL_HTTPS}://${BACKEND_DOMAIN_HEROKU}/api/v1`;
-export const BACKEND_API_PATH = BACKEND_API_PATH_HEROKU;
+export const BACKEND_API_PATH = process.env.REACT_APP_ENDPOINT;
 const FAKE_BACKEND = true;
 export let IS_BACKEND_HEALTHY = () => {
     if (FAKE_BACKEND) return false;
