050dfce2d5df54a5482d17f7c4b35c4641a2d06a
Hiren Parmar <hiren.parmar@workday.com>
2020-03-07 18:10:47 -0800

    Removing Database config.

--- src/main/java/com/hiren/product/expenses/configurations/DatabaseConfiguration.java
+++ src/main/java/com/hiren/product/expenses/configurations/DatabaseConfiguration.java
@@ -1,12 +1,7 @@
 package com.hiren.product.expenses.configurations;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-
-@Configuration
-@ConfigurationProperties("spring.datasource")
+//@Configuration
+//@ConfigurationProperties("spring.datasource")
 public class DatabaseConfiguration {
 
     private String url;
@@ -14,24 +9,24 @@ public class DatabaseConfiguration {
     private String username;
     private String password;
 
-    @Bean
-    @Profile("test")
+    // @Bean
+    // @Profile("test")
     public String testDataSource() {
         System.out.println("URL " + url);
         System.out.println("ClassName " + driveClassName);
         return "Test Data Source in use";
     }
 
-    @Bean
-    @Profile("dev")
+    // @Bean
+    // @Profile("dev")
     public String devDataSource() {
         System.out.println("URL " + url);
         System.out.println("ClassName " + driveClassName);
         return "Dev Data Source in use";
     }
 
-    @Bean
-    @Profile("live")
+    // @Bean
+    // @Profile("live")
     public String dataSource() {
         System.out.println("URL " + url);
         System.out.println("ClassName " + driveClassName);
